ddd727a88609241f34c9fd11df5c45562153f27d
Henry Lee <henryleeworld@gmail.com>
2020-04-14 19:41:11 +0800

    feat:升級至 laravel 7

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,10 +23,9 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    { 
+    {
         $timezoneCookie = request()->cookie('timezone'); 
         $timezone = (in_array($timezoneCookie, DateTimeZone::listIdentifiers())) ? $timezoneCookie : ((auth()->check()) ? auth()->user()->timezone : geoip()->getLocation(request()->ip())->timezone);
         config(['user.timezone' => $timezone]);
-        // date_default_timezone_set($timezone);
     }
 }
